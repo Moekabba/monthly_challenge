3dd667750847a7eeb58ee658e276e9143d221757
Kabba (US) <mahamadou.kabba@boeing.com>
2026-04-28 09:40:03 -0700

    change view to use dict instead of list

--- challenges/views.py
+++ challenges/views.py
@@ -25,16 +25,27 @@ from django.http import HttpResponse, HttpResponseNotFound
 # def junindex(request):
 #     return HttpResponse("Summer time!!Every massive haffi come along Go fi your sun tan inna di summer sun If you a bleacher, go back home ")
 
+month_challenges_dict = {
+    "january": "This january, 1st month from your dict",
+    "februray": "this February, your birthday month",
+    "march": "this is Absa's birthday",
+    "Aprill": "This when Absa mom cone",
+    "May": "Baby due date month",
+    "June": "this is June, stay focus",
+    "July": "July is when you will be good at what you do",
+    "Aguest": "baby will be 3 months Insha Allah",
+    "September": "get fit with Absa",
+    "October": "get fit and stay fit",
+    "November": "prepare to go see mom",
+    "December": "Take break, reset and go see mom"
+
+}
+
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 
 def monthly_challenges(request, month):
-    challenge_txt = None
-    if month == "january":
-        challenge_txt = "This january, 1st month"
-    elif month == "february":
-        challenge_txt = "This is February, your birthday month"
-    else:
-        return HttpResponseNotFound(" month not found")
+    challenge_txt = month_challenges_dict
+  
     return HttpResponse(challenge_txt)
